b86044a0aa9c2f7114e15cc27969bbd4647e90b8
andy <admazzola@gmail.com>
2025-01-07 12:07:48 -0500

    verifying contracts

--- packages/contracts/deploy/upgrades/999_validate_contract_deployments.ts
+++ packages/contracts/deploy/upgrades/999_validate_contract_deployments.ts
@@ -45,6 +45,23 @@ const deployFn: DeployFunction = async (hre) => {
 
   const zeroAddress = "0x0000000000000000000000000000000000000000";
 
+
+
+  const tellerV2ProtocolOwner = await smartCommitmentForwarder.owner()
+
+  
+  hre.log( " ------- "  )
+  hre.log( "SCF owner: "  )
+  hre.log(  scfOwner  )
+  hre.log( " ------- "  )
+ 
+  if (scfOwner == zeroAddress) {
+    throw "Validation Error : SCF owner not defined " 
+  }
+
+
+
+
   const scfOwner = await smartCommitmentForwarder.owner()
 
   
