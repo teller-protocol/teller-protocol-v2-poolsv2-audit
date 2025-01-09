aa3390d6f49ff3f2e088b868897829bdc60ff43f
andy <admazzola@gmail.com>
2025-01-09 10:59:29 -0500

    base deploy

--- packages/contracts/deploy/upgrades/22_upgrade_tellerv2_lender_groups.ts
+++ packages/contracts/deploy/upgrades/22_upgrade_tellerv2_lender_groups.ts
@@ -127,7 +127,7 @@ deployFn.dependencies = [
 deployFn.skip = async (hre) => {
   return (
     !hre.network.live ||
-    !['mainnet', 'arbitrum', 'polygon', 'goerli', 'mainnet_live_fork'].includes(hre.network.name)
+    !['mainnet', 'arbitrum', 'base', 'polygon', 'goerli', 'mainnet_live_fork'].includes(hre.network.name)
   )
 }
 export default deployFn
