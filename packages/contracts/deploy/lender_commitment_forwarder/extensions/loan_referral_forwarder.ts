20c4f48a114d0ebf15fb652dc9f336e48904a723
andy <admazzola@gmail.com>
2025-01-29 15:34:51 -0500

    prep deploy scrips

--- packages/contracts/deploy/lender_commitment_forwarder/extensions/loan_referral_forwarder.ts
+++ packages/contracts/deploy/lender_commitment_forwarder/extensions/loan_referral_forwarder.ts
@@ -1,26 +1,25 @@
 import { DeployFunction } from 'hardhat-deploy/dist/types'
 
-const aavePoolAddressProvider: { [networkName: string]: string } = {
-  mainnet: '0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e',
-  goerli: '0xC911B590248d127aD18546B186cC6B324e99F02c',
-  sepolia: '0x0496275d34753A48320CA58103d5220d394FF77F',
-  polygon: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
-  arbitrum: '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb',
-  base: '0xe20fCBdBfFC4Dd138cE8b2E6FBb6CB49777ad64D',
+
+const uniswapV3Factory: { [networkName: string]: string } = {
+  mainnet: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+  polygon: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+  arbitrum: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+  base: '0x33128a8fC17869897dcE68Ed026d694621f6FDfD',
 }
 
-const networksWithAave: string[] = Object.keys(aavePoolAddressProvider)
+const networksWithUniswap: string[] = Object.keys(uniswapV3Factory)
 
 const deployFn: DeployFunction = async (hre) => {
   const tellerV2 = await hre.contracts.get('TellerV2')
    
 
-  hre.log('Deploying Loan Referral Forwarder...')
+  hre.log('Deploying Loan Referral Forwarder V2...')
 
 
   const networkName = hre.network.name
 
-  const LoanReferralForwarder = await hre.deployProxy('LoanReferralForwarder', {
+  const LoanReferralForwarder = await hre.deployProxy('LoanReferralForwarderV2', {
     unsafeAllow: ['constructor', 'state-variable-immutable'],
     constructorArgs: [
       await tellerV2.getAddress(),
@@ -33,13 +32,13 @@ const deployFn: DeployFunction = async (hre) => {
 }
 
 // tags and deployment
-deployFn.id = 'lender-commitment-forwarder:extensions:loan-referral-forwarder:deploy'
+deployFn.id = 'lender-commitment-forwarder:extensions:loan-referral-forwarder-v2:deploy'
 deployFn.tags = [
   'lender-commitment-forwarder',
   'lender-commitment-forwarder:extensions',
   'lender-commitment-forwarder:extensions:deploy',
   'lender-commitment-forwarder:extensions:loan-referral-forwarder',
-  'lender-commitment-forwarder:extensions:loan-referral-forwarder:deploy',
+  'lender-commitment-forwarder:extensions:loan-referral-forwarder-v2:deploy',
 ]
 deployFn.dependencies = [
   'teller-v2:deploy',
@@ -47,6 +46,6 @@ deployFn.dependencies = [
 ]
 
 deployFn.skip = async (hre) => {
-  return !hre.network.live || !networksWithAave.includes(hre.network.name)
+  return !hre.network.live || !networksWithUniswap.includes(hre.network.name)
 }
 export default deployFn
